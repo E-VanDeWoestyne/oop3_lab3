0a968ef71d3031ca8141d17a205946d6450c2780
Ethan Van De Woestyne <ethan.van@protonmail.com>
2025-03-03 12:20:21 -0700

    Adds initial method contracts

--- src/utilities/DictionaryADT.java
+++ src/utilities/DictionaryADT.java
@@ -1,16 +1,26 @@
 package utilities;
 
 /**
-* DictionaryADT.java
-*
-* @author your name
-* @version major.minor revision number starting at 1.0
-* 
-* Class Definition: This interface represents the public contract for the
-* implementation of Dictionary for the DictionaryADT Lab. 
-*/
+ * DictionaryADT.java
+ *
+ * @author your name
+ * @version major.minor revision number starting at 1.0
+ * 
+ *          Class Definition: This interface represents the public contract for
+ *          the implementation of Dictionary for the DictionaryADT Lab.
+ */
 
-public interface DictionaryADT<K,V>
+public interface DictionaryADT<K, V>
 {
+	int size();
 
+	boolean isEmpty();
+
+	boolean containsKey(Object Key);
+
+	V get(Object key);
+
+	V put(K key, V value);
+
+	V remove(Object key);
 }
