56d68108bdea12db321fbbfbc2fcab63aa6ea08d
MacelroyDev <macehowald@gmail.com>
2025-03-06 17:28:13 -0700

    added javadocs for size isEmpty and containsKey

--- src/utilities/DictionaryADT.java
+++ src/utilities/DictionaryADT.java
@@ -3,8 +3,8 @@ package utilities;
 /**
  * DictionaryADT.java
  *
- * @author your name
- * @version major.minor revision number starting at 1.0
+ * @author Mace Howald
+ * @version 1.1
  * 
  *          Class Definition: This interface represents the public contract for
  *          the implementation of Dictionary for the DictionaryADT Lab.
@@ -12,15 +12,55 @@ package utilities;
 
 public interface DictionaryADT<K, V>
 {
+	/**
+	 * Returns the number of key-value pairs in the dictionary
+	 * 
+	 * @return the number of key-value pairs in the dictionary
+	 * 
+	 * Post: Returns the size of the dictionary
+	 */
 	int size();
-
+	
+	/**
+	 * Returns true if the dictionary is empty
+	 * 
+	 * @return true if the dictionary is empty
+	 * 
+	 * Post: Returns true if the dictionary is empty, false if not.
+	 */
 	boolean isEmpty();
-
+	
+	
+	/**
+	 * Returns true if the dictionary contains a specified key.
+	 * 
+	 * @param Key The specified to key to check for in the dictionary
+	 * @return true if the specified key is found, false if not.
+	 * 
+	 * Pre: Key is not null.
+	 * Post: Returns true if the specified key is found, false if not.
+	 */
 	boolean containsKey(Object Key);
 
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
 	V get(Object key);
 
+	/**
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
 	V put(K key, V value);
-
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
 	V remove(Object key);
 }
