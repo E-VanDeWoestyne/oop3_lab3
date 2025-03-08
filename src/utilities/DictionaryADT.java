47a6029edaf981c83e3240dd1b143b76852d334a
Ethan Van De Woestyne <ethan.van@protonmail.com>
2025-03-07 18:37:42 -0700

    Adds javadoc for get(), put(), and remove()

--- src/utilities/DictionaryADT.java
+++ src/utilities/DictionaryADT.java
@@ -3,7 +3,7 @@ package utilities;
 /**
  * DictionaryADT.java
  *
- * @author Mace Howald
+ * @author Mace Howald, Ethan Van De Woestyne
  * @version 1.1
  * 
  *          Class Definition: This interface represents the public contract for
@@ -43,24 +43,39 @@ public interface DictionaryADT<K, V>
 	boolean containsKey(Object Key);
 
 	/**
-	 * 
-	 * @param key
-	 * @return
+	 * Returns the value associated with the specified key in the dictionary.
+	 *
+	 * @param key The key whose associated value is to be returned
+	 * @return The value associated with the specified key, or null if the key is not found
+	 *
+	 * Pre: Key is not null.
+	 * Post: Returns the value associated with the specified key, or null if the key is not found.
 	 */
 	V get(Object key);
 
 	/**
-	 * 
-	 * @param key
-	 * @param value
-	 * @return
+	 * Associates the specified value with the specified key in the dictionary, or
+	 * updates it if the key already exists.
+	 *
+	 * @param key The key with which the specified value is to be associated
+	 * @param value The value to be associated with the specified key
+	 * @return The previous value associated with the key, or null if there was no mapping
+	 *
+	 * Pre: Key and value are not null.
+	 * Post: The key-value pair is stored in the dictionary. If the key already existed,
+	 *       its value is updated and the old value is returned.
 	 */
 	V put(K key, V value);
 	
 	/**
-	 * 
-	 * @param key
-	 * @return
+	 * Removes the key-value pair for the specified key from the dictionary if present.
+	 *
+	 * @param key The key whose mapping is to be removed from the dictionary
+	 * @return The value previously associated with the key, or null if the key is not found
+	 *
+	 * Pre: Key is not null.
+	 * Post: If the key was found, the key-value pair is removed from the dictionary
+	 *       and the value is returned. If the key was not found, null is returned.
 	 */
 	V remove(Object key);
 }
